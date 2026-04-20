3692aca9e1df23077f95c46e322f52fbfbb84480
Natalia Panova <143701784+Khlipochenko@users.noreply.github.com>
2026-04-20 14:07:05 +0200

    Update GoogleLoginButton.jsx

--- frontend/src/components/auth/GoogleLoginButton.jsx
+++ frontend/src/components/auth/GoogleLoginButton.jsx
@@ -3,15 +3,15 @@ import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { FcGoogle } from 'react-icons/fc'; // Google-Icon von react-icons
 const GoogleLoginButton = () => {
-  const { googleLogin } = useContext(AuthContext); // useContext sichern
-  const handleGoogleLogin = async () => {
-    try {
-      console.log('Starte Google-Login...'); // Debug-Log
-      window.location.href = `${import.meta.env.VITE_API_URL}/api/users/google`;
-    } catch (error) {
-      console.error('Fehler beim Google-Login:', error);
-    }
-  };
+//  const { googleLogin } = useContext(AuthContext); // useContext sichern
+//  const handleGoogleLogin = async () => {
+//    try {
+//      console.log('Starte Google-Login...'); // Debug-Log
+ //     window.location.href = `${import.meta.env.VITE_API_URL}/api/users/google`;
+//    } catch (error) {
+//      console.error('Fehler beim Google-Login:', error);
+ //   }
+//  };
   return (<></>
   //  <button
  //     onClick={handleGoogleLogin}
